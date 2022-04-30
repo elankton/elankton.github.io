605a3318747cba5b7a1ea1b325813218d9f55fa5
elankton <74224289+elankton@users.noreply.github.com>
2022-04-30 08:25:36 +0300

    Create jquery5.js

---
+++ jquery5.js
@@ -0,0 +1,4 @@
+if(document.getElementById("center_middle_add")){
+    document.getElementById("center_middle_add").innerHTML='<a href="https://bit.ly/39sJ4zI"><img title="Join Cointiply to Earn Free Bitcoin!" alt="Earn Free Bitcoin" src="https://cointiply.com/img/promo/banners/1_728x90.gif" width=600" height="80"></a>'; 
+    document.getElementById("center_middle_add").setAttribute("id", "div_top1915");
+}
