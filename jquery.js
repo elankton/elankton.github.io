57ad20bd56707eb8d35011aa31a7861907e36cbe
elankton <74224289+elankton@users.noreply.github.com>
2022-06-21 22:26:25 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,6 +1,7 @@
-var hostname = location.hostname;
+var vhostname = location.hostname;
+vhostname = vhostname.replace(".blogspot.com", "");
 if(!Histats_variables){var Histats_variables=[];}
-Histats_variables.push("tags",hostname);
+Histats_variables.push("tags",vhostname);
 var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4435132,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
