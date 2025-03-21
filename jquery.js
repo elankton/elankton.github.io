f81b395de315c9701d328e2ea3c929794eb99f96
elankton <74224289+elankton@users.noreply.github.com>
2025-03-21 13:23:09 +0500

    update jquery.js

--- jquery.js
+++ jquery.js
@@ -23,7 +23,7 @@ async function Tutor() {
 		script.type = 'text/javascript';
 		script.text = "atOptions = {'key' : '04ab7bf8059140f2e892fdc85a5bb04d','format' : 'iframe','height' : 90,'width' : 728,'params' : {} 	};";
 		var el = document.getElementById("adsyed");
-		el.appendChild(script);
+		el.innerHTML(script);
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = 'https://www.highperformanceformat.com/04ab7bf8059140f2e892fdc85a5bb04d/invoke.js';
@@ -36,7 +36,7 @@ async function Tutor() {
 		script.type = 'text/javascript';
 		script.text = "atOptions = {'key' : '4af9baae14a71c16d7c9c5b12bb59fdb','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
 		var el = document.getElementById("adsucyuz");
-		el.appendChild(script);
+		el.innerHTML(script);
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = 'https://www.highperformanceformat.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
@@ -48,7 +48,7 @@ async function Tutor() {
 		var script = document.createElement('center');
 		script.id = 'a458d2acc';
 		var el = document.getElementById("ads");
-		el.appendChild(script);
+		el.innerHTML(script);
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.text = "atOptions = {'key' : 'a458d2acccabadb0ad769a3f1eb6a238','format' : 'iframe','height' : 60,'width' : 468,'params' : {} 	};";
@@ -68,7 +68,7 @@ async function Tutor() {
 		script.type = 'text/javascript';
 		script.text = "atOptions = {'key' : '4af9baae14a71c16d7c9c5b12bb59fdb','format' : 'iframe','height' : 250,'width' : 300,'params' : {} 	};";
 		var el = document.getElementById("adsucyuz");
-		el.appendChild(script);
+		el.innerHTML =(script);
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = 'https://www.highperformanceformat.com/4af9baae14a71c16d7c9c5b12bb59fdb/invoke.js';
